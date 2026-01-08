f591e260d84962913acde3dbf732008acca6cb5f
N4thh <nhatahh2003@gmail.com>
2026-01-08 22:00:05 +0900

    create skeleton project

---
+++ my-project/src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+
+      <main>
+        <h1> Hello this is my first word</h1>
+      </main>
+      
+    </div>
+  );
+}
